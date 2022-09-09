84a21e850dbd8ca0dde0bb83732eb2d995a11501
Muhammad Zaid <richardmmeans322@gmail.com>
2022-09-09 19:18:26 +0700

    Remove unused css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import { CanvasProvider } from "./context/canvas";
 import { GlobalStyles } from 'twin.macro'
